eb318ad8fc4508abca0e63f19b80f4bc6dd18cac
孤豹 <yongbao.fyb@koubei.com>
2018-05-25 20:39:57 +0800

    fix: add api

--- src/concat.js
+++ src/concat.js
@@ -25,55 +25,55 @@ module.exports = () => {
     }
     return componentList;
   };
-  
+
   const getDocument = (componentList) => {
     const document = {};
-    for (const key in componentList) {
+    for (const key in componentList) { // eslint-disable-line
       document[key] = `${componentList[key].path}/document`;
     }
     return document;
   };
-  
+
   const getAlias = (componentList) => {
     const alias = {};
-    for (const key in componentList) {
+    for (const key in componentList) { // eslint-disable-line
       const name = `@alipay/${key}`;
       alias[name] = `${componentList[key].path}/src`;
     }
     return alias;
   };
-  
+
   const entryTemplate = fs.readFileSync(path.join(process.cwd(), 'site/bisheng-pc.nunjucks.js')).toString();
   const entryPath = `${path.join(process.cwd(), 'site/bisheng-pc2.config.js')}`;
   const entryWapTemplate = fs.readFileSync(path.join(process.cwd(), 'site/bisheng-wap.nunjucks.js')).toString();
   const entryWapPath = `${path.join(process.cwd(), 'site/bisheng-wap2.config.js')}`;
-  
+
   const componentList = getComponentList();
   const document = getDocument(componentList);
   const alias = getAlias(componentList);
 
   // console.log(entryPath);
   // console.log(entryWapPath);
-  
+
   // console.log(`document: ${JSON.stringify(document, null, 2)}`);
   // console.log(`componentList: ${JSON.stringify(componentList, null, 2)}`);
   // console.log(`alias: ${JSON.stringify(alias, null, 2)}`);
-  
+
   fs.writeFileSync(
     entryPath,
     nunjucks.renderString(entryTemplate, {
       document: JSON.stringify(document),
       componentList: JSON.stringify(componentList),
       alias: JSON.stringify(alias),
-    }),
+    })
   );
-  
+
   fs.writeFileSync(
     entryWapPath,
     nunjucks.renderString(entryWapTemplate, {
       document: JSON.stringify(document),
       componentList: JSON.stringify(componentList),
       alias: JSON.stringify(alias),
-    }),
+    })
   );
 };
